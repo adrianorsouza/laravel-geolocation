378788843eb01828d8f4ec5638afbacd6e93b57c
Adriano Rosa <info@adrianorosa.com>
2019-08-13 22:22:10 -0300

    add ConfigTest

---
+++ tests/ConfigTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Adrianorosa\GeoLocation\Tests;
+
+/**
+ * Class ConfigTest.
+ *
+ * @author Adriano Rosa <https://adrianorosa.com>
+ * @date 2019-08-13 20:36
+ *
+ * @package Adrianorosa\GeoLocation\Tests
+ */
+class ConfigTest extends TestCase
+{
+    public function testGetConfigValues()
+    {
+        $this->assertNotNull(config('geolocation.drivers.default'));
+        $this->assertNotNull(config('geolocation.providers.ipinfo.access_token'));
+        $this->assertNotNull(config('geolocation.cache.ttl'));
+
+        $this->assertEquals(4096, config('geolocation.cache.ttl'));
+        $this->assertEquals('LftDDkrDdJtxhA', config('geolocation.providers.ipinfo.access_token'));
+    }
+
+}
